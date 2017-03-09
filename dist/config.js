5b8d782ff0416c37d54445ce08233d9784f477b9
andypinet <lbc19920631@163.com>
2017-03-10 07:55:34 +0800

    测试

--- dist/config.js
+++ dist/config.js
@@ -1,12 +1,18 @@
 self.$config = {
   debug: true,
+  nav: [
+    {title: '2017', type: 'dropdown', items: [
+      {title: '3月', path: '/2017.3/README'}
+    ]}
+  ],  
   plugins: [
     function myPlugin(context) {
       // context.store
       // context.router
       // context....
-      console.dir(context);
-      window.test = context;
+      context.event.on('content:updated', function () {
+		    new LazyLoad();        
+      })      
     }
   ]
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var twig = require('gulp-twig');
 var rename = require("gulp-rename");
+var sass = require('gulp-ruby-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('templates', function() {
-  gulp.src('./src/*.twig')
+  gulp.src('./src/**/*.twig')
     .pipe(twig())
     .pipe(rename(function(path) {
       path.extname = ".md"
@@ -12,5 +14,15 @@ gulp.task('templates', function() {
 });
 
 gulp.task('watch-tpl', function() {
-  gulp.watch('./src/*.twig', ['templates']);
+  gulp.watch('./src/**/*.twig', ['templates']);
 });
+
+gulp.task('sass', () =>
+    sass('src/style.scss')
+        .on('error', sass.logError)
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))        
+        .pipe(gulp.dest('dist'))
+);
