d5158c4767a69eca4f27ae3ea75b03d529dc7ae4
andypinet <lbc19920631@163.com>
2017-03-15 15:33:32 +0800

    test

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var twig = require('gulp-twig');
 var rename = require("gulp-rename");
 var sass = require('gulp-ruby-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var through = require('through2'); 
+var yaml = require('js-yaml');
 
 gulp.task('templates', function() {
   gulp.src('./src/**/*.twig')
@@ -10,7 +12,7 @@ gulp.task('templates', function() {
     .pipe(rename(function(path) {
       path.extname = ".md"
     }))
-    .pipe(gulp.dest('./dist/'))
+    .pipe(gulp.dest('./dist'))
 });
 
 gulp.task('watch-tpl', function() {
@@ -26,3 +28,30 @@ gulp.task('sass', () =>
         }))        
         .pipe(gulp.dest('dist'))
 );
+
+var globalSearch = [];
+
+var handlerSearch = function() {
+  function handlerFile(file) {
+    var path = file.history[0];
+    var contents = file.contents.toString();
+    var mar = contents.match(/<\s*sfi((\s+[^<>]+\s*>)|(\s*>))[^<>]*<\s*\/sfi\s*>/g);    
+    var def = "";
+    if (mar && mar.length > 0) {
+      def = mar[0].replace('<sfi>', '').replace('</sfi>', '').trim();
+      globalSearch.push(Object.assign({
+        relativePath: path.replace(file.base, '')
+      }, yaml.load(def)));
+      console.dir(globalSearch);
+    }
+  }
+
+  return through.obj(function(file, encoding, callback) {
+    callback(null, handlerFile(file));
+  });
+}
+
+gulp.task('sfi', () => {
+  gulp.src("dist/**/*.md")
+    .pipe(handlerSearch())
+});
